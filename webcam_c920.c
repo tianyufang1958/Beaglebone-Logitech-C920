438dd2c1f16d045e5da04a937497aa644c8aa977
root <tianyufang1958@gmail.com>
2015-04-22 14:14:24 +0100

    Image capture from C920

---
+++ webcam_c920.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+
+
+int main()
+{
+    CvCapture *capture = 0;
+    IplImage  *frame = 0;
+        int       key = 0;
+
+    /* initialize camera */
+    capture = cvCaptureFromCAM( 0 );
+    cvSetCaptureProperty( capture, CV_CAP_PROP_FRAME_WIDTH, 640 );
+    cvSetCaptureProperty( capture, CV_CAP_PROP_FRAME_HEIGHT, 480 );
+    
+     /* always check */
+    if ( !capture ) {
+        fprintf( stderr, "Cannot open initialize webcam!\n" );
+        return 1;
+    }
+
+    /* create a window for the video */
+    cvNamedWindow( "result", CV_WINDOW_AUTOSIZE );
+
+    while( key != 'q' ) {
+        /* get a frame */
+        frame = cvQueryFrame( capture );
+
+        /* always check */
+        if( !frame ) break;
+    
+        /* display current frame */
+        cvShowImage( "result", frame );
+    
+        /* exit if user press 'q' */
+        key = cvWaitKey( 1 );
+    }
+
+    /* free memory */
+    cvDestroyWindow( "result" );
+    cvReleaseCapture( &capture );
+    return 0;
+}
